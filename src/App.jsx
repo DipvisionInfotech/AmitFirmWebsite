4a9e8fccfa407483866188da78506b6659cf26bf
SONU JHA <flamethe06@gmail.com>
2025-08-16 22:02:07 +0530

    Add basename to Router and create custom 404 page for redirection

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/AmitFirmWebsite">
         <ScrollToTop />
         <Navbar />
         <Routes>
